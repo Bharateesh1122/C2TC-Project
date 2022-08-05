52e1b223fee9854a1337787fb2c9ecb8cd2e678f
BHARATISH <BHARATISH@DESKTOP-TPD345Q>
2022-08-06 00:50:42 +0530

    first commit

---
+++ Fundamentals/src/com/cg/fundamentals/LeftShift.java
@@ -0,0 +1,16 @@
+package com.cg.fundamentals;
+
+public class LeftShift {
+
+	public static void main(String[] args) {
+		int i;
+		int num =16;
+		
+		for(i=0; i<4; i++)
+		num = num >> 1;
+		System.out.println(num);
+		
+
+	}
+
+}
