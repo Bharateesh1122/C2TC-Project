fd1a9d3027ab8928942f1731be73008191db6875
BHARATISH <BHARATISH@DESKTOP-TPD345Q>
2022-08-05 23:34:18 +0530

    first commit

---
+++ Fundamentals/src/com/cg/fundamentals/PerAndPostlncrement.java
@@ -0,0 +1,16 @@
+package com.cg.fundamentals;
+
+public class PerAndPostlncrement {
+
+	public static void main(String[] args) {
+		int a=5,b=18;
+		if((a=3)==b)
+		{
+			System.out.println(++a);
+			
+		}
+		else
+		{System.out.println(++b);}
+	}
+
+}
