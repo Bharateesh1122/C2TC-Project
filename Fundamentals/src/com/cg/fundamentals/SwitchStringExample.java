7188dcd8146d71aa359c20b2183e9b239d336f00
BHARATISH <BHARATISH@DESKTOP-TPD345Q>
2022-08-08 21:56:21 +0530

    first commit

---
+++ Fundamentals/src/com/cg/fundamentals/SwitchStringExample.java
@@ -0,0 +1,24 @@
+package com.cg.fundamentals;
+
+public class SwitchStringExample {
+
+	public static void main(String[] args) {
+		String currentDay="Friday";
+		
+		switch(currentDay) {
+		case"Monday":
+		case"Tuesday":
+		case"Wenesday":
+			System.out.println("boring day");
+		case"Thrusday":
+			System.out.println("grtting better");
+		case"Friday":
+			System.out.println("much better");
+		case"saturday":
+		case"sunday":
+			
+		}
+
+	}
+
+}
