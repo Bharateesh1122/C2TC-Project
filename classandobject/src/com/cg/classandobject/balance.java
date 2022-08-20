9a0a64a54fb02a86fc02af5c791c11ce85926a10
BHARATISH <BHARATISH@DESKTOP-TPD345Q>
2022-08-20 18:05:04 +0530

    first commit

---
+++ classandobject/src/com/cg/classandobject/balance.java
@@ -0,0 +1,20 @@
+package com.cg.classandobject;
+
+public class balance {
+         private String name;
+         private double balance;
+         
+         
+         public Balance(String name, double balance) {
+        	 super();
+        	 this.name = name;
+        	 this.balance = balance;
+         }
+         
+         public void show() {
+        	 if(balance<0)
+        		 System.out.println("your balance is low");
+        	 System.out.println(" -->>");
+        	 System.out.println(name + ":$" + balance);
+         }
+}
