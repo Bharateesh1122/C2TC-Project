f3d9826245283db84fa94906770e05352ff522da
BHARATISH <BHARATISH@DESKTOP-TPD345Q>
2022-08-20 20:07:41 +0530

    first commit

---
+++ classandobject/src/com/cg/classandobject/Box.java
@@ -0,0 +1,56 @@
+package com.cg.classandobject;
+
+public class Box {
+	
+	
+	double dblWidth;
+	double dblHeight;
+	double dblDepth;
+	
+	public Box(  ) {
+		super();
+		
+	}
+
+	
+	public Box(double dblWidth, double dblHeight,double dblDepth) {
+		
+	super();
+	this.dblWidth = dblWidth;
+	this.dblHeight = dblHeight;
+	this.dblDepth = dblDepth;
+		
+
+	}
+
+	double clacVolume() {
+		return dblWidth * dblHeight * dblDepth;
+	}
+	
+	public double getDblWidth() {
+		return dblWidth;
+	}
+	
+	public void setDlbWidth(double dblWidth() {
+		this.dblWidth = =dblWidth;
+		
+	}
+	
+	public double getDblHeight() {
+		return dblHeight;
+		
+	}
+	
+	public void setDblHeight(double dblHeight) {
+		this.dblHeight = dblHeight;
+	}
+	
+	public double getDblDepth() {
+		return dblDepth;
+	}
+	
+	public void setDblDepth(double dblDepth) {
+		this.dblDepth = dblDepth;
+		
+	}
+}
