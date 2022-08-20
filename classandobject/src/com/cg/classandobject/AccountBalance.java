d41268bc0f49c75b885042a4ea9a8de7408c7c3a
BHARATISH <BHARATISH@DESKTOP-TPD345Q>
2022-08-20 18:09:42 +0530

    first commit

---
+++ classandobject/src/com/cg/classandobject/AccountBalance.java
@@ -0,0 +1,9 @@
+package com.cg.classandobject;
+
+public class AccountBalance {
+     public static void main(String arg[]) {
+    	 
+    	 Balance currten = new Balance("K.j.Fielding", -1);
+    	 current.show();
+     }
+}
